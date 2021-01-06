87f711d3d9bde0c25d22642669fb22bca8af00d9
my_home <kch6506@gmail.com>
2021-01-07 00:10:59 +0900

    se commit

--- app/Listeners/ArticlesEventListener.php
+++ app/Listeners/ArticlesEventListener.php
@@ -43,7 +43,7 @@ class ArticlesEventListener
     {
       if($event->action==='created')
       {
-        \Log::info(sprintf('새로운 포럼 글이 등록되었습니다. : %s',$event->article->title));
+        Log::info(sprintf('새로운 포럼 글이 등록되었습니다. : %s',$event->article->title));
       }
     }
 }
